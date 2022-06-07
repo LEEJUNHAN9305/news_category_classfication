39fe2fa2c17abbbb5b85a24024a9bab967097094
이준한 <54476630+LEEJUNHAN9305@users.noreply.github.com>
2022-06-07 17:15:34 +0900

    news_category_classification_preprocessing

--- job04_preprocessing.py
+++ job04_preprocessing.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from konlpy.tag import Okt
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.utils import to_categorical
+from keras.utils import to_categorical
 import pickle
 
 pd.set_option('display.unicode.east_asian_width', True)
